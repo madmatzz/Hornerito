d12502a0dbf7cca87346a30936f06d202c693db1
madmatzz <matimatzkin.la37@gmail.com>
2025-03-20 21:14:33 -0300

    Fix Next.js Link component usage in layout

--- components/kokonutui/layout.tsx
+++ components/kokonutui/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -11,9 +12,9 @@ export function Layout({ children, className = '' }: LayoutProps) {
             <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
                 <div className="container flex h-14 items-center">
                     <div className="mr-4 flex">
-                        <a className="mr-6 flex items-center space-x-2" href="/">
+                        <Link className="mr-6 flex items-center space-x-2" href="/">
                             <span className="font-bold">Hornerito</span>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </header>
